000e95fa1de0f147b8d5fce299d2b6e5045b9b14
Liubomyr Chumak <lubomyr.chumak@gmail.com>
2020-06-08 03:25:03 +0200

    Added csv files with input data

--- main0.2.py
+++ main0.2.py
@@ -1,17 +1,23 @@
 import PySimpleGUI as sg
 import csv
+import os
+
+# csv files
+EX_file = 'expensedata.csv'
+IN_file = 'incomedata.csv'
 
 sg.theme('Light blue')
 
 # layouts
 
-main_layout = [[sg.Text('Hello, what you would to do?',),
-                sg.Text(size=(15,1), key='-OUTPUT-', )],
+main_layout = [[sg.Text('Hello, what you would to do?', ),
+                sg.Text(size=(15, 1), key='-OUTPUT-', )],
                [sg.Button('Add expense'), sg.Button('Add income'), sg.Button('Quit')]]
 
 # windows
 
-main_window = sg.Window("Expense tracker", main_layout, grab_anywhere=True, size=(400, 300), text_justification='center')
+main_window = sg.Window("Expense tracker", main_layout, grab_anywhere=True, size=(400, 300),
+                        text_justification='center')
 win2_active = False
 win3_active = False
 i = 0
@@ -23,14 +29,15 @@ while True:
         print(i, event, values)
     if event in (sg.WIN_CLOSED, 'Quit'):
         break
-    elif event == 'Add expense'and not win2_active:
+    elif event == 'Add expense' and not win2_active:
         win2_active = True
         layout_2 = [
             [sg.Text('How much money you spend that time?', justification='center'),
              sg.Text(size=(15, 1), key='-OUTPUT-')],
-            [sg.Text("Expense", relief=sg.RELIEF_RAISED), sg.Input(key='-EXPENSE-', background_color='white',
-            text_color='black')],
-            [sg.CalendarButton("Data"), sg.Input(key='-DATA-', background_color='white', text_color='black')],
+            [sg.Text("Expense"), sg.Input(key='-EXPENSE-', background_color='white',
+                                          text_color='black')],
+            [sg.CalendarButton("Data", format='%d.%m.%y'),
+             sg.Input(key='-DATA-', background_color='white', text_color='black')],
             [sg.Text("Description"), sg.Input(key='-DESCRIPTION-', background_color='white', text_color='black')],
             [sg.Button('Add expense'), sg.Button('Back')]
         ]
@@ -40,23 +47,27 @@ while True:
         if event != sg.TIMEOUT_KEY:
             print("win2 ", event)
         if event == 'Add expense':
-            fields = ['Expense', 'Data', 'Description']
-            with open(r'name', 'a') as f:
-                writer = csv.writer(f)
-                writer.writerow(fields)
-            sg.popup('Your expense successfully added')
+            with open(EX_file, 'w', newline='') as f:
+                fieldnames = ['Expense', 'Data', 'Description']
+                thewriter_1 = csv.DictWriter(f, fieldnames=fieldnames, delimiter='|')
+                thewriter_1.writeheader()
+                thewriter_1.writerow({'Expense': values['-EXPENSE-'],
+                                      'Data': values['-DATA-'],
+                                      'Description': values['-DESCRIPTION-']})
+                sg.popup('Your expense successfully added')
             window2.close()
         if event == 'Back' or event == sg.WIN_CLOSED:
             win2_active = False
             window2.close()
-    elif event == 'Add income'and not win3_active:
+    elif event == 'Add income' and not win3_active:
         win3_active = True
         layout_3 = [
             [sg.Text('How much money you earned that time?', justification='center'),
              sg.Text(size=(15, 1), key='-OUTPUT-')],
-            [sg.Text("Earned", relief=sg.RELIEF_RAISED), sg.Input(key='-EARN-', background_color='white',
-            text_color='black')],
-            [sg.CalendarButton("Data"), sg.Input(key='-DATA-', background_color='white', text_color='black')],
+            [sg.Text("Earned"), sg.Input(key='-EARN-', background_color='white',
+                                                                text_color='black')],
+            [sg.CalendarButton("Data", format='%d.%m.%y'),
+             sg.Input(key='-DATA-', background_color='white', text_color='black')],
             [sg.Text("Description"), sg.Input(key='-DESCRIPTION-', background_color='white', text_color='black')],
             [sg.Button('Add income'), sg.Button('Back')]
         ]
@@ -66,14 +77,22 @@ while True:
         if event != sg.TIMEOUT_KEY:
             print("win3 ", event)
         if event == 'Add income':
+            with open(IN_file, 'w', newline='') as fi:
+                fieldnames = ['Earned', 'Data', 'Description']
+                thewriter_2 = csv.DictWriter(fi, fieldnames=fieldnames, delimiter='|')
+                thewriter_2.writeheader()
+                thewriter_2.writerow({'Earned': values['-EARN-'],
+                                      'Data': values['-DATA-'],
+                                      'Description': values['-DESCRIPTION-']})
             sg.popup('Your earned money successfully added')
             window3.close()
+            with open(IN_file, newline='') as file:
+                spamreader = csv.reader(file, delimiter=' ', quotechar='|')
+                for row in spamreader:
+                    print(' '.join(row))
+
         if event == 'Back' or event == sg.WIN_CLOSED:
             win3_active = False
             window3.close()
-
-
-
-
 
 main_window.close()
