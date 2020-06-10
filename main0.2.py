d346980f951131ee4e4b70d7b9c0c25a54887961
Liubomyr Chumak <lubomyr.chumak@gmail.com>
2020-06-10 02:53:42 +0200

    Fixed issues with writing in csv files

--- main0.2.py
+++ main0.2.py
@@ -5,6 +5,8 @@ import sys
 import pandas
 import subprocess
 
+# 1. Create a showstats window
+
 
 # csv files
 EX_file = 'expensedata.csv'
@@ -13,21 +15,17 @@ IN_file = 'incomedata.csv'
 # theme
 sg.theme('Light blue')
 
-
-
 # menu toolbar
 
 menu_def = [['File', ['Open', 'Exit']],
             ['Help', 'About...'], ]
 
-
-
-main_layout =[   [sg.Menu(menu_def, tearoff=True)],
+main_layout = [[sg.Menu(menu_def, tearoff=True)],
                [sg.Text('Hello, what you want to do?', size=(50, 1), font=30)],
                [sg.Button('Add expense', border_width=3, pad=((0, 0), (20, 0))),
                 sg.Button('Add income', border_width=3, pad=((75, 0), (20, 0)))],
                [sg.Button('Quit', border_width=3, pad=((240, 0), (60, 0)))]
-            ]
+               ]
 
 # windows
 main_window = sg.Window("Expense tracker", main_layout, grab_anywhere=False, size=(300, 200))
@@ -47,8 +45,8 @@ while True:
     elif event == 'Open' and not menu_window:
         menu_window = True
         event, values = sg.Window('Find files', [[sg.Text('What file you want open?')], [sg.InputText(key='-PATH-'),
-                                                                                     sg.FileBrowse()],
-                                             [sg.OK(), sg.Cancel()]], size=(450, 150)).read(close=True)
+                                                                                         sg.FileBrowse()],
+                                                 [sg.OK(), sg.Cancel()]], size=(450, 150)).read(close=True)
         source = values['-PATH-']
         subprocess.call(["xdg-open", source])
 
@@ -56,9 +54,10 @@ while True:
         win2_active = True
 
         layout_2 = [[sg.Text('How much money you spend that time?', justification='center'), sg.Text(size=(15, 1))],
-                   [sg.Text("Expense"), sg.Input(key='-EXPENSE-', background_color='white',
-                    text_color='black', pad=(16, 0))], [sg.CalendarButton("Data", format='%d.%m.%y'),
-                    sg.Input(key='-DATA-', background_color='white', text_color='black', pad=(16, 0))],
+                    [sg.Text("Expense"), sg.Input(key='-EXPENSE-', background_color='white',
+                                                  text_color='black', pad=(16, 0))],
+                    [sg.CalendarButton("Data", format='%d.%m.%y'),
+                     sg.Input(key='-DATA-', background_color='white', text_color='black', pad=(16, 0))],
                     [sg.Text("Description"), sg.Input(key='-DESCRIPTION-', background_color='white', text_color='black',
                                                       pad=(0, 0))],
                     [sg.Button('Add expense'), sg.Button('Back', pad=(10, 0))]]
@@ -69,14 +68,14 @@ while True:
             if event != sg.TIMEOUT_KEY:
                 print('win2', event)
             if event == 'Add expense':
-                with open(EX_file, 'w', newline='') as f:
+                with open(EX_file, 'a', newline='') as f:
                     fieldnames = ['Expense', 'Data', 'Description']
-                    thewriter_1 = csv.DictWriter(f, fieldnames=fieldnames, delimiter=' ')
-                    thewriter_1.writeheader()
-                    thewriter_1.writerow({'Expense': values['-EXPENSE-'],
-                                      'Data': values['-DATA-'],
-                                      'Description': values['-DESCRIPTION-']})
+                    writer_1 = csv.DictWriter(f, fieldnames=fieldnames)
+                    writer_1.writerow({'Expense': values['-EXPENSE-'],
+                                          'Data': values['-DATA-'],
+                                          'Description': values['-DESCRIPTION-']})
                 sg.popup('Your expense successfully added')
+                win2_active = False
                 window2.close()
             if event == 'Back' or event == sg.WIN_CLOSED:
                 win2_active = False
@@ -87,10 +86,12 @@ while True:
 
         layout_3 = [[sg.Text('How much money you earned that time?', justification='center'), sg.Text(size=(15, 1))],
                     [sg.Text("Earned"), sg.Input(key='-EARN-', background_color='white',
-                    text_color='black', pad=(25, 0))], [sg.CalendarButton("Data", format='%d.%m.%y'),
-                    sg.Input(key='-DATA-', background_color='white', text_color='black', pad=(16, 0))],
+                                                 text_color='black', pad=(25, 0))],
+                    [sg.CalendarButton("Data", format='%d.%m.%y'),
+                     sg.Input(key='-DATA-', background_color='white', text_color='black', pad=(16, 0))],
                     [sg.Text("Description"), sg.Input(key='-DESCRIPTION-', background_color='white', text_color='black',
-                    pad=(0, 0))],[sg.Button('Add income'), sg.Button('Back', pad=(10, 0))]]
+                                                      pad=(0, 0))],
+                    [sg.Button('Add income'), sg.Button('Back', pad=(10, 0))]]
 
         window3 = sg.Window('Add income', layout_3, size=(450, 150))
         if win3_active:
@@ -98,20 +99,16 @@ while True:
             if event != sg.TIMEOUT_KEY:
                 print('win3', event)
             if event == 'Add income':
-                with open(IN_file, 'w', newline='') as fi:
+                with open(IN_file, 'a', newline='') as fi:
                     fieldnames = ['Earned', 'Data', 'Description']
-                    thewriter_2 = csv.DictWriter(fi, fieldnames=fieldnames, delimiter='|')
-                    thewriter_2.writeheader()
-                    thewriter_2.writerow({'Earned': values['-EARN-'],
-                                      'Data': values['-DATA-'],
-                                      'Description': values['-DESCRIPTION-']})
+                    writer_2 = csv.DictWriter(fi, fieldnames=fieldnames)
+                    writer_2.writerow({'Earned': values['-EARN-'], 'Data': values['-DATA-'],
+                                                    'Description': values['-DESCRIPTION-']})
                 sg.popup('Your earned money successfully added')
+                win3_active = False
                 window3.close()
             if event == 'Back' or event == sg.WIN_CLOSED:
                 win3_active = False
                 window3.close()
 
 main_window.close()
-
-
-
